78e010bb84d4a9337400e5c76403df1b14a30eb9
senatede <bhupalo@kse.org.ua>
2025-05-13 18:57:50 +0300

    implemented file logic without length limits, made unlimited size of the input, text rows and columns

--- main.c
+++ main.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void func1(char text[][100], unsigned int NumberOfLines);
-void func2(unsigned int *NumberOfLines);
-void func3();
-void func4();
-void func5(char text[][100], unsigned int NumberOfLines);
+#define INITIAL_BUFFER_SIZE 32
+#define INITIAL_ROW_COUNT 10
+
+char* console_input();
+void func1(char** text);
+void func2(char*** text);
+void func3(char** text);
+char** func4();
+void func5(char** text);
 void func6();
 void func7();
 
-char command[2];
-unsigned int NumberOfLines = 1;
-char text[10][100] = { 0 };
+char command[3];
+unsigned int LastLine = 0;
+unsigned int N_rows = INITIAL_ROW_COUNT;
+unsigned int BufferSize = INITIAL_BUFFER_SIZE;
+FILE* file;
 
 int main(void) {
-    printf("Welcome to the Simple Text Editor!\n Available commands:\n 1 - Append text symbols to the end\n 2 - Start the new line\n 5 - Print the current text to console\n");
+    char **text = calloc(N_rows, sizeof(char*));
+    text[LastLine] = malloc(1);
+    text[LastLine][0] = '\0'; // creating first empty line
+
+    printf("Welcome to the Simple Text Editor!\n Available commands:\n 1 - Append text symbols to the end\n 2 - Start the new line\n 3 - Save to file\n 4 - Load from file\n 5 - Print the current text to console\n");
     while(1) {
         printf("Choose the command:\n");
         scanf("%2s", command);
@@ -22,25 +33,105 @@ int main(void) {
         if (strlen(command) != 1) {printf("Command is not implemented.\n"); continue;}
 
         switch (*command) {
-            case '1': func1(text, NumberOfLines); break;
-            case '2': func2(&NumberOfLines); break;
-            case '5': func5(text, NumberOfLines); break;
+            case '1': func1(text); break;
+            case '2': func2(&text); break;
+            case '3': func3(text); break;
+            case '4': text = func4(); break;
+            case '5': func5(text); break;
             default: printf("Command is not implemented.\n"); break;
         }
     }
 }
 
-void func1(char text[][100], const unsigned int NumberOfLines) {
-    char new_text[100];
-    printf("Enter text to append: ");
-    fgets(new_text, sizeof(new_text), stdin);
-    new_text[strlen(new_text)-1] = 0; // removing \n at the end
-    strcat(text[NumberOfLines-1], new_text);
+char* console_input() { // Text input
+    size_t size = INITIAL_BUFFER_SIZE;
+    size_t len = 0;
+    char* buffer = malloc(size);
+    char c;
+    while ((c = getchar()) != '\n') {
+        buffer[len++] = c;
+        if (len == size) {
+            size *= 2;
+            buffer = realloc(buffer, size);
+        }
+    }
+    buffer[len] = '\0';
+    return buffer;
 }
-void func2(unsigned int *NumberOfLines) {
-    (*NumberOfLines) = (*NumberOfLines) + 1;
+
+void func1(char** text) { // Adding text
+    printf("Enter text to append: ");
+    char* str = console_input();
+    if (text[LastLine] == NULL) text[LastLine] = malloc(strlen(str) + 1);
+    else {
+        size_t new_len = strlen(text[LastLine]) + strlen(str) + 1;
+        text[LastLine] = realloc(text[LastLine], new_len);
+    }
+    strcat(text[LastLine], str);
+    free(str);
+}
+void func2(char*** text) { // New line
+    if (LastLine >= N_rows) {
+        N_rows *= 2;
+        *text = realloc(*text, N_rows * sizeof(char*));
+    }
+    LastLine++;
+    (*text)[LastLine] = malloc(1);
+    (*text)[LastLine][0] = '\0';
     printf("New line is started\n");
 }
-void func5(char text[][100], const unsigned int NumberOfLines) {
-    for (int i = 0; i < NumberOfLines; i++) printf("%s\n", text[i]);
+void func3(char** text) { // Save to file
+    printf("Enter the file name for saving: ");
+    char *file_name = console_input();
+    file = fopen(file_name, "w");
+    if (file != NULL) {
+        for (int i = 0; i <= LastLine; i++) fprintf(file, (i != LastLine) ? "%s\n" : "%s", text[i]);
+        fclose(file);
+        printf("Text has been saved successfully\n");
+        free(file_name);
+    }
+    else {
+        printf("File name cannot be empty.\n");
+    }
+}
+char** func4() { // Load from file
+    LastLine = 0;
+    N_rows = INITIAL_ROW_COUNT;
+
+    printf("Enter the file name for loading: ");
+    const char* file_name = console_input();
+    file = fopen(file_name, "r");
+    if (file == NULL) printf("Error opening file\n");
+    else
+    {
+        char **text = calloc(N_rows, sizeof(char*));
+        char c;
+        size_t size = INITIAL_BUFFER_SIZE;
+        text[LastLine] = malloc(size);
+        size_t len = 0;
+        while ((c = fgetc(file)) != EOF) {
+            if (c != '\n') {
+                text[LastLine][len++] = c;
+                if (len == size) {
+                    size *= 2;
+                    text[LastLine] = realloc(text[LastLine], size);
+                }
+            }
+            else {
+                text[LastLine][len] = '\0';
+                size = INITIAL_BUFFER_SIZE;
+                len = 0;
+                if (LastLine >= N_rows) {
+                    N_rows *= 2;
+                    *text = realloc(*text, N_rows * sizeof(char*));
+                }
+                LastLine++;
+                text[LastLine] = malloc(size);
+            }
+        }
+        return text;
+    }
+}
+void func5(char** text) { // Print text
+    for (int i = 0; i <= LastLine; i++) printf("%s\n", text[i]);
 }
