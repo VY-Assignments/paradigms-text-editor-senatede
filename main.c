c7b3a295af46fdbe4afb3eaad2e380c4a68f479f
senatede <bhupalo@kse.org.ua>
2025-05-18 22:44:39 +0300

    final touches

--- main.c
+++ main.c
@@ -8,14 +8,13 @@
 void print_help();
 char* console_input();
 void func1(char** text);
-void func2(char*** text);
+void func2(char*** text_pointer);
 void func3(char** text);
-char** func4(char** text);
+void func4(char*** text_pointer);
 void func5(char** text);
-void func6(char*** text);
+void func6(char*** text_pointer);
 void func7(char** text);
 
-char command[3];
 int LastLine = 0;
 unsigned int N_rows = INITIAL_ROW_COUNT;
 unsigned int BufferSize = INITIAL_BUFFER_SIZE;
@@ -30,16 +29,15 @@ int main(void) {
     print_help();
     while(1) {
         printf("Choose the command:\n");
-        scanf("%2s", command);
-        while (getchar() != '\n');
-        system("clear");
+        char* command = console_input();
+        system("clear"); // Clearing console
         if (strlen(command) != 1) {printf("Command is not implemented.\n"); continue;}
 
         switch (*command) {
             case '1': func1(text); break;
             case '2': func2(&text); break;
             case '3': func3(text); break;
-            case '4': text = func4(text); break;
+            case '4': func4(&text); break;
             case '5': func5(text); break;
             case '6': func6(&text); break;
             case '7': func7(text); break;
@@ -47,6 +45,7 @@ int main(void) {
             case 'h': print_help(); break;
             default: printf("Command is not implemented.\n"); break;
         }
+        free(command);
     }
 }
 
@@ -67,10 +66,10 @@ char* console_input() { // Text input
     size_t len = 0;
     char* buffer = malloc(size);
     char c;
-    while ((c = getchar()) != '\n') {
+    while ((c = getchar()) != '\n') { // getting each character
         buffer[len++] = c;
         if (len == size) {
-            size *= 2;
+            size *= 2; // doubling size of bugger
             buffer = realloc(buffer, size);
         }
     }
@@ -83,19 +82,19 @@ void func1(char** text) { // Adding text
     if (text[LastLine] == NULL) text[LastLine] = malloc(strlen(str) + 1);
     else {
         size_t new_len = strlen(text[LastLine]) + strlen(str) + 1;
-        text[LastLine] = realloc(text[LastLine], new_len);
+        text[LastLine] = realloc(text[LastLine], new_len); // reallocating memory for new line length
     }
     strcat(text[LastLine], str);
     free(str);
 }
-void func2(char*** text) { // New line
+void func2(char*** text_pointer) { // New line
     if (LastLine >= N_rows) {
-        N_rows *= 2;
-        *text = realloc(*text, N_rows * sizeof(char*));
+        N_rows *= 2; // doubling number of rows
+        *text_pointer = realloc(*text_pointer, N_rows * sizeof(char*));
     }
     LastLine++;
-    (*text)[LastLine] = malloc(1);
-    (*text)[LastLine][0] = '\0';
+    (*text_pointer)[LastLine] = malloc(1);
+    (*text_pointer)[LastLine][0] = '\0';
     printf("New line is started\n");
 }
 void func3(char** text) { // Save to file
@@ -112,51 +111,51 @@ void func3(char** text) { // Save to file
         printf("File name cannot be empty.\n");
     }
 }
-char** func4(char** text) { // Load from file
-    LastLine = 0;
-    N_rows = INITIAL_ROW_COUNT;
-
+void func4(char*** text_pointer) { // Load from file
     printf("Enter the file name for loading: ");
-    const char* file_name = console_input();
+    char* file_name = console_input();
     file = fopen(file_name, "r");
     if (file == NULL) {
         printf("Error opening file\n");
-        return text;
     }
-    free(text);
-    char **text = calloc(N_rows, sizeof(char*));
-    char c;
-    size_t size = INITIAL_BUFFER_SIZE;
-    text[LastLine] = malloc(size);
-    size_t len = 0;
-    while ((c = fgetc(file)) != EOF) {
-        if (c != '\n') {
-            text[LastLine][len++] = c;
-            if (len == size) {
-                size *= 2;
-                text[LastLine] = realloc(text[LastLine], size);
+    else {
+        LastLine = 0;
+        N_rows = INITIAL_ROW_COUNT;
+        free(*text_pointer);
+        *text_pointer = calloc(N_rows, sizeof(char*));
+        char c;
+        size_t size = INITIAL_BUFFER_SIZE;
+        (*text_pointer)[LastLine] = malloc(size);
+        size_t len = 0;
+        while ((c = fgetc(file)) != EOF) {
+            if (c != '\n') {
+                (*text_pointer)[LastLine][len++] = c;
+                if (len == size) {
+                    size *= 2; // doubling size for now
+                    (*text_pointer)[LastLine] = realloc((*text_pointer)[LastLine], size);
+                }
+            }
+            else {
+                (*text_pointer)[LastLine][len] = '\0';
+                (*text_pointer)[LastLine] = realloc((*text_pointer)[LastLine], len + 1); // reallocating for the exact line length
+                size = INITIAL_BUFFER_SIZE;
+                len = 0;
+                if (LastLine >= N_rows-1) {
+                    N_rows *= 2; // doubling the number of lines
+                    *text_pointer = realloc(*text_pointer, N_rows * sizeof(char*));
+                }
+                LastLine++;
+                (*text_pointer)[LastLine] = malloc(size); // allocating memory for the next line
             }
         }
-        else {
-            text[LastLine][len] = '\0';
-            text[LastLine] = realloc(text[LastLine], len + 1);
-            size = INITIAL_BUFFER_SIZE;
-            len = 0;
-            if (LastLine >= N_rows) {
-                N_rows *= 2;
-                *text = realloc(*text, N_rows * sizeof(char*));
-            }
-            LastLine++;
-            text[LastLine] = malloc(size);
-        }
+        printf("Text has been loaded successfully\n");
     }
-    printf("Text has been loaded successfully\n");
-    return text;
+    free(file_name);
 }
 void func5(char** text) { // Print text
     for (int i = 0; i <= LastLine; i++) printf("%s\n", text[i]);
 }
-void func6(char*** text) { // Insert text by row and symbol index
+void func6(char*** text_pointer) { // Insert text by row and symbol index
     int row, col;
     printf("Choose line and index: ");
     if (scanf("%d %d", &row, &col) != 2 || row < 0 || col < 0) {
@@ -164,29 +163,29 @@ void func6(char*** text) { // Insert text by row and symbol index
         while (getchar() != '\n');
         return;
     }
-    while (getchar() != '\n');
+    while (getchar() != '\n'); // clearing console buffer
     printf("Enter text to insert: ");
     char* str = console_input();
     if (row > LastLine) {
         if (row >= N_rows) {
-            N_rows = row + 1;
-            *text = realloc(*text, N_rows * sizeof(char*));
+            N_rows = row + 1; // changing the number of lines
+            *text_pointer = realloc(*text_pointer, N_rows * sizeof(char*));
         }
-        for (int i = LastLine+1; i < row; i++) {
-            (*text)[i] = malloc(1);
-            (*text)[i][0] = '\0';
+        for (int i = LastLine+1; i < row; i++) { // creating empty lines between last one and the one we want
+            (*text_pointer)[i] = malloc(1);
+            (*text_pointer)[i][0] = '\0';
         }
-        (*text)[row] = malloc(col + strlen(str) + 1);
-        for (int i = 0; i < col; i++) (*text)[row][i] = ' ';
-        strcat((*text)[row], str);
+        (*text_pointer)[row] = malloc(col + strlen(str) + 1); // allocating memory for our line
+        for (int i = 0; i < col; i++) (*text_pointer)[row][i] = ' '; // fitting spaces
+        strcat((*text_pointer)[row], str);
         LastLine = row;
     }
     else {
-        int new_len = (col > strlen((*text)[row])) ? col + strlen(str) + 1: strlen((*text)[row]) + strlen(str) + 1;
-        (*text)[row] = realloc((*text)[row], new_len);
-        for (int i = strlen((*text)[row]); i > col; i--) (*text)[row][i-1+strlen(str)] = (*text)[row][i-1];
-        for (int i = strlen((*text)[row]); i < col; i++) (*text)[row][i] = ' ';
-        for (int i = 0; i < strlen(str); i++) (*text)[row][col+i] = str[i];
+        int new_len = (col > strlen((*text_pointer)[row])) ? col + strlen(str) + 1: strlen((*text_pointer)[row]) + strlen(str) + 1;
+        (*text_pointer)[row] = realloc((*text_pointer)[row], new_len); // reallocating for new len
+        for (int i = strlen((*text_pointer)[row]); i > col; i--) (*text_pointer)[row][i-1+strlen(str)] = (*text_pointer)[row][i-1]; // moving characters after index to the right
+        for (int i = strlen((*text_pointer)[row]); i < col; i++) (*text_pointer)[row][i] = ' '; // if index is > than len of the line we fit empty space with spaces
+        for (int i = 0; i < strlen(str); i++) (*text_pointer)[row][col+i] = str[i];
     }
     free(str);
 }
@@ -194,29 +193,29 @@ void func7(char** text) { // Substring search mechanism
     printf("Enter text to search: ");
     char* str = console_input();
     int row_index = 0; int col_index = 0;
-    int match = 0;
-    int count = 0;
+    int match_count = 0;
+    int matches = 0;
     printf("Text is present in this positions: ");
     for (int row = 0; row <= LastLine; row++) {
         for (int index = 0; index < strlen(text[row]); index++) {
-            if (text[row][index] == str[match]) {
-                if (match == 0) {
+            if (text[row][index] == str[match_count]) {
+                if (match_count == 0) { // if it's the first match we set the index of the start
                     row_index = row;
                     col_index = index;
                 }
-                match++;
-                if (match == strlen(str)) {
+                match_count++;
+                if (match_count == strlen(str)) { // if the match_count == len of substring then we found a match
                     printf("%d %d; ", row_index, col_index);
-                    row_index = col_index = match = 0;
-                    count++;
+                    row_index = col_index = match_count = 0;
+                    matches++;
                 }
             }
             else {
-                index = index - match;
-                match = 0;
+                index = index - match_count; // going back to the next index after the start of match
+                match_count = 0;
             }
         }
     }
-    printf("\nTotal matches: %d\n", count);
+    printf("\nTotal matches: %d\n", matches);
     free(str);
 }
